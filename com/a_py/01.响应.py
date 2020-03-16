1d73ebaa42d9e032e4c07b0956bb34823f7c6aa7
jerry.zhou <jerry.zhou@ptn.advantest.com>
2020-03-17 00:38:18 +0800

    jinja2

--- com/a_py/01.响应.py
+++ com/a_py/01.响应.py
@@ -1,4 +1,38 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify, render_template
+
+app = Flask(__name__)
+
+
+# 响应 html
+@app.route('/')
+def test():
+    return '''
+    <h1>Hello html</h1>
+    '''
+
+
+# 重定向
+@app.route('/readhtml')
+def readhtml():
+    return redirect('https://www.baidu.com')
+
+
+# 响应 json
+@app.route('/strjson')
+def strjson():
+    user = {'id': 1, 'name': 'jiahua', 'age': 18, 'sex': 'male'}
+    return jsonify(user)
+
+
+# 响应页面
+@app.route('/temp')
+def temp():
+    return render_template('hello.html', title='Hello python')
+
+
+if __name__ == '__main__':
+    port = 12306;
+    app.run(port=port, debug=True)
---
+++ com/a_py/jinja2_temp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    users = [
+        {'id': 1, 'name': 'jiahua', 'age': 18, 'sex': 'male'},
+        {'id': 2, 'name': 'Jerry', 'age': 28, 'sex': 'male'},
+        {'id': 3, 'name': 'Jack', 'age': 38, 'sex': 'male'}
+    ]
+    return render_template('home.html', msg='Welcome to home page', users=users)
+
+
+# 全局异常处理
+# @app.errorhandler(404)
+# def error_hand():
+#     return render_template('404.html')
+
+
+if __name__ == '__main__':
+    port = 12306;
+    app.run(port=port, debug=True)
